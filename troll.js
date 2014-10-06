9ec9cf4efc74d20e5d4a8f6013097216001c4ffc
Eric <cioleker@msu.edu>
2014-10-05 22:40:38 -0400

    all instances of console.log -> alert in troll.js

--- troll.js
+++ troll.js
@@ -7,30 +7,30 @@ switch(troll) {
     var strong = prompt("How courageous! Are you strong (YES or NO)?").toUpperCase();
     var smart = prompt("Are you smart?").toUpperCase();
     if(strong === 'YES' || smart === 'YES') {
-      console.log("You only need one of the two! You beat the troll--nice work!");
+      alert("You only need one of the two! You beat the troll--nice work!");
     } else {
-      console.log("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a troll. You lose!");
+      alert("You're not strong OR smart? Well, if you were smarter, you probably wouldn't have tried to fight a troll. You lose!");
     }
     break;
   case 'PAY':
     var money = prompt("All right, we'll pay the troll. Do you have any money (YES or NO)?").toUpperCase();
     var dollars = prompt("Is your money in Troll Dollars?").toUpperCase();
     if(money === 'YES' && dollars === 'YES') {
-      console.log("Great! You pay the troll and continue on your merry way.");
+      alert("Great! You pay the troll and continue on your merry way.");
     } else {
-      console.log("Dang! This troll only takes Troll Dollars. You get whomped!");
+      alert("Dang! This troll only takes Troll Dollars. You get whomped!");
     }
     break;
   case 'RUN':
     var fast = prompt("Let's book it! Are you fast (YES or NO)?").toUpperCase();
     var headStart = prompt("Did you get a head start?").toUpperCase();
     if(fast === 'YES' || headStart === 'YES') {
-      console.log("You got away--barely! You live to stroll through the forest another day.");
+      alert("You got away--barely! You live to stroll through the forest another day.");
     } else {
-      console.log("You're not fast and you didn't get a head start? You never had a chance! The troll eats you.");
+      alert("You're not fast and you didn't get a head start? You never had a chance! The troll eats you.");
     }
     break;
   default:
-    console.log("Error: input not recognized. Hit Run and try again, this time picking FIGHT, PAY, or RUN!");
+    alert("Error: input not recognized. Hit Run and try again, this time picking FIGHT, PAY, or RUN!");
   };
 };
